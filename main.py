d0182781a4b414ebdc653226b248b731513f0711
Jossua Figueiras <jossufigueiras@icloud.com>
2023-12-16 15:22:36 +0100

    ajout des fonction qui donnent les paramètres de la fenêtre et ajout l'image au background

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 from tkinter import *
 
 def create_principal_window():
+    """Cette fonction revoie la fenêtre dont les paramètres sont
+    défini"""
     window = Tk()
-    set_window_setting()
+    set_window_setting(window)
+    add_background(window)
+    return window
 
-def set_window_setting():
+def set_window_setting(window):
+    """Cette fonction donne les paramètres à la fenêtre et prend 
+    en paramètre la fenêtre"""
+    window.title("Jeu de carte")
+    window.geometry("1200x700")
+    
+def add_background(window):
+    """ajoute une image au format ppm en fond de la page mise en 
+    paramètre de cette fonction"""
+    image = PhotoImage(file="plateau-en-bois.ppm")
+    label = Label(window, image=image)
+    label.place(x=0, y=0, relwidth=1, relheight=1)
 
 
-# Créer la fenêtre principale
+window = create_principal_window()
 
-
-# on place l'image en background
-image = PhotoImage(file="plateau-en-bois.ppm")  # Remplacez par le chemin de votre image en format PPM
-label = Label(window, image=image)
-label.place(x=0, y=0, relwidth=1, relheight=1)
 
 
 
