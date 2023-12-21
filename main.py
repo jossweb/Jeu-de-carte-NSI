90babd2566db4fee99435aebfdfc25f4f2fa9f69
Jossua Figueiras <jossufigueiras@icloud.com>
2023-12-21 21:13:07 +0100

    ajout des dimension de la fenêtre de jeu à la fonction set window settings et adaptations des autres fonction pour intégrer ce nouvel écran

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ def create_principal_window():
     """Cette fonction revoie la fenêtre dont les paramètres sont
     défini"""
     window = Tk()
-    set_window_setting(window)
+    set_window_setting(window, "home")
     window.resizable(False, False)
     return window
 
-def set_window_setting(window):
+def set_window_setting(window, type):
     """Cette fonction donne les paramètres à la fenêtre et prend 
     en paramètre la fenêtre"""
-    window.title("Jeu de carte")
-    window_postion = get_window_start_position(window)
-    window.geometry(f"1600x800+{window_postion[0]}+{window_postion[1]}")
+    if (type == "home"):
+        window.title("Jeu de carte")
+        window_postion = get_window_start_position(window, 1000, 600)
+        window.geometry(f"1000x600+{window_postion[0]}+{window_postion[1]}")
+    elif(type == "game"):
+        window.title("Jeu de carte")
+        window_postion = get_window_start_position(window, 1920, 1080)
+        window.geometry(f"1920x1080+{window_postion[0]}+{window_postion[1]}")
+    else:
+        print("ERROR : Window type is not defined or unknown")
 
-def get_window_start_position(window):
+def get_window_start_position(window, window_width, window_height):
     """Fonction qui renvoie une liste qui contient 2 entier qui vont 
     servir a placer la fenêtre au centre de l'écran de l'utilisateur"""
     #récupération de la taille de l'écran de l'utisateur en pixel
@@ -23,7 +30,7 @@ def get_window_start_position(window):
     screen_height = window.winfo_screenheight()
     #ici l'on divise par 2 les longeurs précédentes et on leurs enlèvent
     #la moitié de la longeur de leurs axe (x ou y)
-    return [(int(screen_width / 2)) - 800, (int(screen_height / 2)) - 400]
+    return [(int(screen_width / 2)) - int(window_width / 2), (int(screen_height / 2)) - int(window_height / 2)]
     
 def add_background(window):
     """ajoute une image au format ppm en fond de la page mise en 
