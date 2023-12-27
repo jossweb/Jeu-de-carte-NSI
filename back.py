bc22313bbf6669d8e2a867c25f067126a5b46e3d
Jossua Figueiras <jossufigueiras@icloud.com>
2023-12-27 13:23:01 +0100

    avancements sur  la fonction main de la page game

--- back.py
+++ back.py
@@ -1,6 +1,20 @@
+
+#modules externes
 from json import loads
+from tkinter import *
+
+#modules internes au projet
+from style import *
 
 def Deserialization_json(json_path):
     with open(json_path, "r") as fichier:
         json_data = fichier.read()
     return loads(json_data)
+
+def create_window(type):
+    """Cette fonction renvoie la fenêtre avec les paramètres qui 
+    sont définis"""
+    window = Tk()
+    set_window_setting(window, type)
+    window.resizable(False, False)
+    return window
--- main.py
+++ main.py
@@ -5,42 +5,49 @@ from tkinter import ttk
 import customtkinter
 
 # import de module internes au projet
-from style import *
+import style
+import back
 
-def create_principal_window():
-    """Cette fonction renvoie la fenêtre dont les paramètres sont définis"""
-    window = Tk()
-    set_window_setting(window, "home")
-    window.resizable(False, False)
-    return window
+#def constantes
+SIZE_WINDOW_WELCOME = [1000, 600]
+SIZE_WINDOW_GAME = [1920, 1080]
 
-def on_button_click():
-    print("Bouton cliqué !")
+def play_button_click():
+    main_game_page()
 
-def __main__():
+def main_welcome_page():
+    """Fonction principal qui affiche les premières information à
+    l'utilisateur. Cette fonction est exécuté au démarage du programme"""
 
-    window = create_principal_window()
+    window = back.create_window("home")
     BACKGROUND_IMAGE = PhotoImage(file="plateau-en-bois.ppm")
 
     #création du Canvas
     canvas = Canvas(window, width=1000, height=600)
     canvas.pack()
 
-    style = ttk.Style()
-    style.configure('TButton', font=('Helvetica', 12), 
-                padding=10, borderwidth=5, relief="flat", 
-                bordercolor="gray", borderround=10)
-
     #ajout de l'image en background
     canvas.create_image(0, 0, anchor=NW, image=BACKGROUND_IMAGE)
     #ajout du texte de bienvenue
     canvas.create_text(500, 50, text="Bienvenue", font="calibri 45 italic", fill="white")
 
-    button = set_button_setting(on_button_click, window, "json/style_button_play.json")
+    button = style.set_button_setting(play_button_click, window, "json/style_button_play.json")
     canvas.create_window(500, 200, window=button)
 
 
     window.mainloop()
 
-#exécution de la fonction principal
-__main__()
+def main_game_page():
+    window = back.create_window("game")
+
+    BACKGROUND_IMAGE = PhotoImage(file="images/point de vue 1.png")
+
+    #création du Canvas
+    canvas = Canvas(window, width=1600, height=900)
+    canvas.pack()
+
+    #ajout de l'image en background
+    canvas.create_image(0, 0, anchor=NW, image=BACKGROUND_IMAGE)
+
+#exécution de la fonction principal au démarage
+main_welcome_page()
